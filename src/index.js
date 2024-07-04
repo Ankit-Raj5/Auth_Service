17882378ece94bb9a29e0c33e565808c0e20d32c
ankit <ankitxrajpoot@gmail.com>
2024-07-04 23:40:42 +0530

    Added get user by id at repository layer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const apiRoutes = require('./routes/index');
 
 // const {User} = require('./models/index');
 // const bcrypt = require('bcrypt');
+const UserRepository = require('./repository/user-repository')
  
 const app = express();
 
@@ -20,9 +21,8 @@ const prepareAndStartServer = () => {
     app.listen(PORT, async () => {
         console.log(`Server Started on PORT : ${PORT}`);
 
-        // const incomingpassword = '1235456';
-        // const user = await User.findByPk(3);
-        // const response = bcrypt.compareSync(incomingpassword, user.password);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
         // console.log(response);
     });
 }
