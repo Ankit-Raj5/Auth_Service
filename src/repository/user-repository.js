0add3a602acf87d3142d5dc17270089bc7698346
ankit <ankitxrajpoot@gmail.com>
2024-07-04 20:18:09 +0530

    signUp logic with Password encryption done

---
+++ src/repository/user-repository.js
@@ -0,0 +1,33 @@
+const { where } = require('sequelize');
+const {User} = require('../models/index');
+
+
+class UserRepository{
+
+    async create(data){
+        try {
+            const user = await User.create(data);
+            return user;
+        } catch (error) {
+            console.log("Something went wrong at repository layer");
+            throw error;
+        }
+    }
+
+    async destroy(userId){
+        try {
+            const user = await User.destroy({
+                where: {
+                    id: userId
+                }
+            });
+            return true;
+        } catch (error) {
+            console.log("Something went wrong at repository layer");
+            throw error;
+        }
+    }
+}
+
+
+module.exports = UserRepository;
